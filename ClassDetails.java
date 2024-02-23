5ed88e4574a8900f752f9009e20fb63721a98068
divakarangit <s.divakaran92@gmail.com>
2024-02-23 12:57:04 +0530

    one

---
+++ ClassDetails.java
@@ -0,0 +1,11 @@
+package org.push1;
+
+public class ClassDetails {
+	public static void main(String[] args) {
+		String s1 = "Java";
+		String s2 = "tutorial";
+		 s1 = s1.concat(s2);
+		System.out.println(s1);
+	}
+
+}
